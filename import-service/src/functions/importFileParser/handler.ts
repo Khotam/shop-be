50033066858619c4620efecff6d76a47036e2155
Khotam <bakhromov17@gmail.com>
2021-09-22 23:09:47 +0500

    fix parser function

--- import-service/src/functions/importFileParser/handler.ts
+++ import-service/src/functions/importFileParser/handler.ts
@@ -10,48 +10,38 @@ import { INTERNAL_SERVER_ERROR } from "src/constants/responseCodes";
 
 const { S3_BUCKET } = process.env;
 const importFileParser = async (event) => {
-  try {
-    const s3 = new S3Client({ region: "eu-west-1" });
+  const s3 = new S3Client({ region: "eu-west-1" });
 
-    for (const record of event.Records) {
-      const { key } = record.s3.object;
-      const s3Params = {
-        Bucket: S3_BUCKET,
-        Key: key,
-      };
-      const { Body } = await s3.send(new GetObjectCommand(s3Params));
-      Body.pipe(csvParser())
-        .on("data", (data) => {
-          console.log(`data`, data);
-        })
-        .on("error", (error) => {
-          console.error("Error[Stream]: ", error);
-        })
-        .on("end", async () => {
-          console.log(`Copy from ${S3_BUCKET}/${key}`);
-          await s3.send(
-            new CopyObjectCommand({
-              Bucket: S3_BUCKET,
-              CopySource: `${S3_BUCKET}/${key}`,
-              Key: key.replace("uploaded", "parsed"),
-            })
-          );
-          console.log(
-            `Copied into ${S3_BUCKET}/${key.replace("uploaded", "parsed")}`
-          );
+  for (const record of event.Records) {
+    const { key } = record.s3.object;
+    const s3Params = {
+      Bucket: S3_BUCKET,
+      Key: key,
+    };
+    const { Body } = await s3.send(new GetObjectCommand(s3Params));
+    Body.pipe(csvParser())
+      .on("data", (data) => {
+        console.log(`data`, data);
+      })
+      .on("error", (error) => {
+        console.log(`key`, key);
+        console.error("Error[Stream]: ", error.message);
+      })
+      .on("end", async () => {
+        console.log(`Copy from ${S3_BUCKET}/${key}`);
+        await s3.send(
+          new CopyObjectCommand({
+            Bucket: S3_BUCKET,
+            CopySource: `${S3_BUCKET}/${key}`,
+            Key: key.replace("uploaded", "parsed"),
+          })
+        );
+        console.log(
+          `Copied into ${S3_BUCKET}/${key.replace("uploaded", "parsed")}`
+        );
 
-          console.log(`Stream ended`);
-        });
-    }
-  } catch (error) {
-    console.error("Error: ", error.message);
-    return formatJSONResponse(
-      {
-        url: null,
-        error: error.message,
-      },
-      INTERNAL_SERVER_ERROR
-    );
+        console.log(`Stream ended`);
+      });
   }
 };
 
