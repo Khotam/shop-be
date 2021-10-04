82cb18ae03d4abdb3dd8def1949c446e1686d298
Khotam <bakhromov17@gmail.com>
2021-10-04 14:21:52 +0500

    task-6 completed

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -3,6 +3,7 @@ import type { AWS } from "@serverless/typescript";
 import getProductsList from "@functions/getProductsList";
 import getProductsById from "@functions/getProductsById";
 import postProduct from "@functions/postProduct";
+import catalogBatchProcess from "@functions/catalogBatchProcess";
 
 const serverlessConfiguration: AWS = {
   service: "product-service",
@@ -30,14 +31,67 @@ const serverlessConfiguration: AWS = {
       PG_DATABASE: "${env:PG_DATABASE}",
       PG_USERNAME: "${env:PG_USERNAME}",
       PG_PASSWORD: "${env:PG_PASSWORD}",
+      SNS_ARN: {
+        Ref: "SNSTopic",
+      },
     },
     lambdaHashingVersion: "20201221",
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: "sqs:*",
+        Resource: {
+          "Fn::ImportValue": "sqs-queue-task6-arn",
+        },
+      },
+      {
+        Effect: "Allow",
+        Action: "sns:*",
+        Resource: {
+          Ref: "SNSTopic",
+        },
+      },
+    ],
   },
   // import the function via paths
   functions: {
     getProductsList,
     getProductsById,
     postProduct,
+    catalogBatchProcess,
+  },
+  resources: {
+    Resources: {
+      SNSTopic: {
+        Type: "AWS::SNS::Topic",
+        Properties: {
+          TopicName: "createProductTopic",
+        },
+      },
+      SNSSubscription: {
+        Type: "AWS::SNS::Subscription",
+        Properties: {
+          Endpoint: "bakhromov17@gmail.com",
+          Protocol: "email",
+          TopicArn: {
+            Ref: "SNSTopic",
+          },
+        },
+      },
+      SNSSubscriptionFilterPolicy: {
+        Type: "AWS::SNS::Subscription",
+        Properties: {
+          Endpoint: "Khotam_Bakhromov@epam.com",
+          Protocol: "email",
+          TopicArn: {
+            Ref: "SNSTopic",
+          },
+          FilterPolicy: {
+            product_type: ["premium"],
+          },
+        },
+      },
+    },
   },
 };
 
